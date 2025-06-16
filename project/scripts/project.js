2471246600b849a9d67fd2ad7e9e8414e6b6695a
willieboat <willieboat2@gmail.com>
2025-06-16 11:26:25 +0000

    week06

---
+++ project/scripts/project.js
@@ -0,0 +1,16 @@
+document.getElementById('bookingForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const vehicle = document.getElementById('vehicle').value;
+    const start = document.getElementById('start').value;
+    const end = document.getElementById('end').value;
+
+    const confirmation = `
+      Thank you, ${name}!<br>
+      Your ${vehicle} is booked from ${start} to ${end}.
+    `;
+
+    document.getElementById('confirmation').innerHTML = confirmation;
+    this.reset();
+});
